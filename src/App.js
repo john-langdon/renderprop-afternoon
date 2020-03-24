16b208d9256b523d6465c8c173618028759d3285
John Langdon <johnlangdon@johns-mbp.home>
2020-03-24 16:47:06 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import CurrencyConverter from "./Components/CurrencyConverter.js";
+import CurrencyDisplay from "./Components/CurrencyDisplay.js";
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
-    );
+      <>
+        <h2>Render Props</h2>
+        <CurrencyConverter render={(currencyData, amount) => ( 
+              // US Dollar ${amount.toFixed(2)} - {currencyData.name}{' '}
+              // {currencyData.symbol}
+              // {(amount * currencyData.rate).toFixed(2)}
+              <CurrencyDisplay currencyData={currencyData} amount={amount} />
+    )} 
+          />
+      </>
+    )
   }
 }
 
